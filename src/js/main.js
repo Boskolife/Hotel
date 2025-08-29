a98c20b0106664b6c52a336c2ae07fbf56eec315
front2-galaxy-it <94291571+Boskolife@users.noreply.github.com>
2025-08-29 19:36:22 +0300

    add: wide slide , wide image sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Navigation, EffectCreative } from 'swiper/modules';
 
 window.addEventListener('load', () => {
   ListenerResize();
@@ -9,7 +9,7 @@ window.addEventListener('resize', () => {
   ListenerResize();
 });
 
-new Swiper('.swiper', {
+new Swiper('.swiper-events', {
   loop: true,
   modules: [Navigation],
   navigation: {
@@ -41,6 +41,38 @@ new Swiper('.swiper', {
     },
   },
 });
+
+const wideSwiper = new Swiper('.wide-swiper', {
+  loop: true,
+  centeredSlides: true,
+  modules: [Navigation, EffectCreative],
+  speed: 2000,
+  slidesPerView: 1,
+  allowTouchMove: false,
+  spaceBetween: 300,
+  effect: 'creative',
+  creativeEffect: {
+    prev: {
+      translate: ['-100%', 0, 0],
+      scale: 1.1,
+    },
+    next: {
+      translate: ['100%', 0, 0],
+      scale: 1.1,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const sliderEl = document.querySelector('.wide-swiper');
+if (sliderEl) {
+  sliderEl.addEventListener('click', () => {
+    wideSwiper.slideNext();
+  });
+}
 
 function ListenerResize() {
   const items = document.querySelectorAll('.scroll-element');
