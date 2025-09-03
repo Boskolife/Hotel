4e3fe2e10b185c06b420c19182ff26531c8099ed
BrassyDANYL <dsgames737@gmail.com>
2025-09-04 00:47:50 +0300

    pushed form

--- src/js/main.js
+++ src/js/main.js
@@ -186,3 +186,26 @@ function initHeaderMenu() {
     });
   }
 }
+
+
+const select = document.querySelector(".form-select");
+  const selected = select.querySelector(".select-selected");
+  const items = select.querySelector(".select-items");
+
+  selected.addEventListener("click", () => {
+    items.classList.toggle("show");
+  });
+
+  items.querySelectorAll("div").forEach(option => {
+    option.addEventListener("click", () => {
+      selected.textContent = option.textContent;
+      selected.dataset.value = option.dataset.value;
+      items.classList.remove("show");
+    });
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!select.contains(e.target)) {
+      items.classList.remove("show");
+    }
+  });
